d9c33e68ba4b8efa0c58965e41bb7e4abb8e4dd8
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2021-12-15 16:48:35 +0200

    Mine vs Rock Prediction Project

--- Mine vs Rock Prediction/tpot_exported_pipeline.py
+++ Mine vs Rock Prediction/tpot_exported_pipeline.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import StandardScaler
-from tpot.builtins import StackingEstimator
-from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,14 +9,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=100)
 
-# Average CV score on the training set was: 0.8779166666666667
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=KNeighborsClassifier(n_neighbors=1, p=2, weights="uniform")),
-    StandardScaler(),
-    KNeighborsClassifier(n_neighbors=4, p=1, weights="uniform")
-)
-# Fix random state for all the steps in exported pipeline
-set_param_recursive(exported_pipeline.steps, 'random_state', 100)
+# Average CV score on the training set was: 0.99375
+exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.9500000000000001, min_samples_leaf=14, min_samples_split=14, n_estimators=100)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
